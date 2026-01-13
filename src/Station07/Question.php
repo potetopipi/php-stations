8d16765c60c116961cfad4f33a375a9f0c84ed2d
poteko <poteko@example.com>
2026-01-13 21:36:11 +0900

    station07

--- src/Station07/Question.php
+++ src/Station07/Question.php
@@ -9,12 +9,22 @@ class Question
         $array1 = ['北海道' => 1, '東京都' => 13, '大阪府' => 'XX'];
         $array2 = ['広島県' => 34, '京都府' => 26];
         $array3 = ['京都府' => 'XX', '大阪府' => 27];
+        $mergedArray = array_merge($array1, $array3, $array2);
+        return $mergedArray;
     }
 
     public function two(): array
     {
         $firstNames = ['太郎', '次郎', '花子'];
         $lastNames = ['山田', '鈴木', '佐藤'];
+
+        $result = [];
+        for($i = 0; $i < 3; $i++){
+            $result[] = $lastNames[$i].$firstNames[$i];
+        }
+        
+
+        return $result;
     }
 
     public function three(): array
@@ -24,5 +34,10 @@ class Question
             ['name' => 'banana', 'price' => 200],
             ['name' => 'orange', 'price' => 120],
         ];
+        $array2 = array_column($array, 'price', 'name');
+        return $array2;
     }
 }
+print_r((new Question())->one());
+print_r((new Question())->two());
+print_r((new Question())->three());
